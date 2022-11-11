d0132db794c962a5f6b3a5ac1ddc2b3dc01b7b15
FranciscoVeloz <franciscoveloz245@gmail.com>
2022-11-11 12:05:22 -0600

    Updating showcase

--- src/components/Home/Showcase.jsx
+++ src/components/Home/Showcase.jsx
@@ -9,7 +9,7 @@ const Showcase = () => {
           Hi, I'm Francisco <span className='txt-primary'>Veloz</span>
         </p>
         <p className='showcase-content'>
-          I am a System Computer Engineer and I am a Fullstack JavaScript/TypeScript Developer with +2 years of
+          I am a System Computer Engineer and I am a Fullstack JavaScript/TypeScript Developer with +3 years of
           experience. My objective is to participate as a backend or frontend developer in high impact projects
           where I can help and solve different problems and situations in a company.
         </p>
